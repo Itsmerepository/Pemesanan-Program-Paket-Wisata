4621e5b79b37043a71fb60f4b7c484f725144264
salmasariii <101516985+salmasariii@users.noreply.github.com>
2022-06-17 00:47:50 +0700

    Update Asistensi IV.py

--- Team Project/Asistensi IV.py
+++ Team Project/Asistensi IV.py
@@ -439,12 +439,8 @@ TANGGAL KEBERANGKATAN : {tanggalkeberangkatan}
 TOTAL HARGA           : Rp. {total:,} ,-
 METODE BAYAR          : {metodes}
 ======================================================================
-<<<<<<< HEAD
 
                                         ''')                                        
-=======
-                                        ''')
->>>>>>> 041cab786af2ac096becbbc838ef256fd47a017c
                                         print("==== Selamat, pesanan Anda telah terkonfirmasi ==== ")
                                         print("         Kami akan segera menghubungi Anda      ")
                                         print("                Terima Kasih                ")
@@ -507,17 +503,10 @@ METODE BAYAR          : {metodes}
                                             datapesan.append(status)
                                             datapesan.append('-')
                                             with open('data.csv', 'a', newline='') as f:
-<<<<<<< HEAD
                                                 writer = csv.writer(f)
                                                 # write the data
                                                 writer.writerow(datapesan)
                                                 f.close()
-=======
-                                                    writer = csv.writer(f)
-                                                    # write the data
-                                                    writer.writerow(datapesan)
-                                                    f.close()
->>>>>>> 041cab786af2ac096becbbc838ef256fd47a017c
                                             break  
                                         else:
                                             print("Data yang Anda Masukkan Salah")
@@ -610,11 +599,7 @@ TANGGAL KEBERANGKATAN : {tanggalkeberangkatan}
 TOTAL HARGA           : Rp. {total:,} ,-
 METODE BAYAR          : {metodes}
 ======================================================================
-<<<<<<< HEAD
                                         ''')                                            
-=======
-                                        ''')
->>>>>>> 041cab786af2ac096becbbc838ef256fd47a017c
                                             datapesan = []
                                             datapesan.append(Nama_pemesan)
                                             datapesan.append(umur_pemesan)
@@ -644,10 +629,7 @@ METODE BAYAR          : {metodes}
                                                 break
                                             except ValueError:
                                                 print("Masukkan dalam bentuk Angka")
-<<<<<<< HEAD
 
-=======
->>>>>>> 041cab786af2ac096becbbc838ef256fd47a017c
                                         print("=================================================================================")
                                     else:
                                         print("===================================")
